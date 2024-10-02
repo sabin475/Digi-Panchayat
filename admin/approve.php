6bce1a5934609e49eb54eedc7ed37cd8fbc079d2
sabin475 <145981331+sabin475@users.noreply.github.com>
2024-10-02 22:57:20 +0530

    Add files via upload

---
+++ admin/approve.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$id=$_GET['id'];
+
+include 'includes/dbconnect.php';
+$sql="UPDATE `login` SET status = '1' WHERE username = '$id'";
+if(mysql_query($sql,$con))
+	echo "<script>alert(' Approved!');location.href='index.php';</script>";
+     else
+     	{"<script>alert('error!');location.href='index.php';</script>";
+		}
+
+?>
