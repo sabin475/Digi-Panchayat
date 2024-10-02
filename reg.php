b7d29547fe73f0cef0b7dc602d3eca7bea25b8ee
sabin475 <145981331+sabin475@users.noreply.github.com>
2024-10-02 22:15:20 +0530

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,251 @@
+   <?php
+   include 'includes/dbconnect.php';
+   
+if(isset($_POST['submit1']))
+{
+	$id=autoid("ureg","uid");
+	
+    $flag=0;
+	//sur,name,gender,hname,hno,sname,city,district,pincode,adar,phno,mailid,password,cpassword
+    
+	$sur=$_POST['sur'];
+    $name=$_POST['name'];
+    $gender=$_POST['gender'];
+    $hname=$_POST['hname'];
+	$hno=$_POST['hno'];
+    $sname=$_POST['sname'];
+    $city=$_POST['city'];
+    $district=$_POST['district'];
+	$pincode=$_POST['pincode'];
+	$adar=$_POST['adar'];
+	$phno=$_POST['phno'];
+	$mailid=$_POST['mailid'];
+	$password=$_POST['password'];
+    $cpassword=$_POST['cpassword'];
+	$date=date('y-m-d');
+	
+    if($password!=$cpassword)
+    {
+        $flag=1;
+    }
+	
+    if($flag==0)
+    {
+        
+        
+        $data="SELECT * FROM ureg natural join login";
+        $c=0;
+        $sd="SELECT * FROM ureg";
+        $p=mysql_query($sd,$con);
+        while($row=mysql_fetch_array($p))
+        {
+            $c++;
+        }
+        $c++;
+        $sq=mysql_query($data,$con);
+        while($row=mysql_fetch_array($sq))
+        {
+            if($row['mailid']==$mailid||$row['phno']==$phno)
+            {
+                $flag=1;
+            }
+        }
+        if($flag==1)
+        {
+            echo"<script>alert('This  account already exits');</script>";
+        }
+        else if($flag==0)
+        {
+            $sql="INSERT INTO `ureg`VALUES ('$id','$sur','$name','$gender','$hname','$hno','$sname','$city','$district','$pincode','$adar','$phno','$mailid')";
+            if(mysql_query($sql,$con))
+            {
+                $sql1="INSERT INTO `login`VALUES ('$mailid','$password','public',0)";
+                if(mysql_query($sql1,$con))
+                {
+                    echo"<script>alert('Account created ! Please login ');</script>";
+                    
+                }
+            }
+        }
+    }
+    else
+    {
+     echo"<script>alert('Password doesn't match);</script>";   
+    }
+}
+            ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"> <title>Digi - Panchayat</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+	<style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <?php 
+		include'includes/head.php';
+		?>
+
+            <div class="container-xxl py-5 bg-primary hero-header mb-5">
+                <div class="container my-5 py-5 px-lg-5">
+                    <div class="row g-5 py-5">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar & Hero End -->
+
+
+        <!-- Full Screen Search Start -->
+        <div class="modal fade" id="searchModal" tabindex="-1">
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content" style="background: rgba(29, 29, 39, 0.7);">
+                    <div class="modal-header border-0">
+                        <button type="button" class="btn bg-white btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body d-flex align-items-center justify-content-center">
+                        <div class="input-group" style="max-width: 600px;">
+                            <input type="text" class="form-control bg-transparent border-light p-3" placeholder="Type search keyword">
+                            <button class="btn btn-light px-4"><i class="bi bi-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Full Screen Search End -->
+
+
+        <!-- Contact Start -->
+        <div class="container-xxl py-5">
+            <div class="container px-lg-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-7">
+                        <div class="section-title position-relative text-center mb-5 pb-2 wow fadeInUp" data-wow-delay="0.1s">
+                            <h6 class="position-relative d-inline text-primary ps-4">Register Now</h6>
+                            <h2 class="mt-2">Registration for public</h2>
+                        </div>
+                        <div class="wow fadeInUp" data-wow-delay="0.3s">
+                            <form action="" method="POST">
+                               <table id="customers">
+							 
+								<tr><td>Surname</td><td><select name="sur"><option>- Select Surname -</option>
+								                                             <option value="Mr.">- Mr. -</option>
+																			 <option value="Mrs.">- Mrs. -</option>
+																			 <option value="Ms.">- Ms. -</option></select></td></tr>
+								<tr><td>Name</td><td><input type="text" name="name" pattern='[a-zA-Z\s]{3,30}' title="Minimum 3 & maximum 30 characters. Letters only permitted."required></td></tr>
+								<tr><td>Gender</td><td><select name="gender"><option>- Select Gender -</option>
+								                                             <option value="Male">- Male -</option>
+																			 <option value="Female">- Female -</option></select></td></tr>
+					            
+								 <tr><td>House Name</td><td><input type="text" name="hname" pattern='[a-zA-Z\s]{3,30}' title="Minimum 3 & maximum 30 characters. Letters only permitted." required></td></tr>
+								 <tr><td>House No.</td><td><input type="text" name="hno" pattern='[a-zA-Z0-9]{3,50}' title="Minimum 3 & maximum 50 characters."required></td></tr>
+								 <tr><td>Street Name</td><td><input type="text" name="sname" pattern='[a-zA-Z]{3,30}' title="Minimum 3 & maximum 30 characters. Letters only permitted." required></td></tr>
+								 <tr><td>City</td><td><input type="text" name="city" pattern='[a-zA-Z]{3,30}' title="Minimum 3 & maximum 30 characters. Letters only permitted." required></td></tr>
+								 <tr><td>District</td><td><input type="text" name="district" pattern='[a-zA-Z]{3,30}' title="Minimum 3 & maximum 30 characters. Letters only permitted." required></td></tr>
+								 <tr><td>Pincode</td><td><input type="text" name="pincode" pattern='[0-9]{6}' title="6 numbers only." required></td></tr>
+								 <tr><td>Aadhar No.</td><td><input type="text" name="adar" pattern='[0-9]{12}' title="12 numbers only."required></td></tr>
+								 <tr><td>Mobile No.</td><td><input type="text" name="phno" pattern='[0-9]{10}' title="10 numbers only."required></td></tr>
+								 <tr><td>Email</td><td><input type="email" name="mailid" required></td></tr>
+								 <tr><td>Password</td><td><input type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></td></tr>
+								 <tr><td>Confirm Password</td><td><input type="password" name="cpassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required></td></tr>
+								 <tr><td><input type="submit" name="submit1" value="Submit"></td>
+								     <td><input type="submit" name="submit2" value="Cancel"></td></tr>
+								</table>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
+        
+
+        <!-- Footer Start -->
+        <?php
+		include'includes/footer.php';
+		?>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
